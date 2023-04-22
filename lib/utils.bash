4899fd7f44dd490fccb795902f8d31b4f0d26508
susurri <susurrus.silent@gmail.com>
2023-04-23 08:19:36 +0900

    implement Linux support

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,8 +2,7 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for seed7.
-GH_REPO="https://github.com/ThomasMertes/seed7"
+SF_REPO="https://sourceforge.net/projects/seed7/files/seed7/"
 TOOL_NAME="seed7"
 TOOL_TEST="s7c -?"
 
@@ -12,28 +11,15 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
-
-# NOTE: You might want to remove this if seed7 is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+curl_opt=-fsSL
 
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if seed7 has other means of determining installable versions.
-	list_github_tags
+	curl "${curl_opt}" "$SF_REPO" | grep 'span class="name">seed7' | sed -e 's/.*seed7_\([0-9_]*\).*/\1/' | sort_versions
 }
 
 download_release() {
@@ -41,17 +27,16 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for seed7
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$SF_REPO/seed7_${version}/seed7_${version}.tgz/download"
 
 	echo "* Downloading $TOOL_NAME release $version..."
-	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	curl "${curl_opt}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="$3"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -61,10 +46,16 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert seed7 executable exists.
+		cd "$install_path"/src
+		make depend
+		make
+		make s7c
+		cd ..
+		rm -rf src
+
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
